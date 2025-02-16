53d5e8f9716762016877e1578333fcc7edc34bcd
Jela <jela3105@gmail.com>
2025-02-16 08:24:42 -0600

    Configuration of enviroment variables

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!!');
+import { envs } from "./config";
+import { Server } from "./presentation/server";
+
+(() => {
+    main();
+})();
+
+async function  main() {
+   // TODO: Await for database connection 
+
+   new Server({
+        port: envs.PORT,
+   }).start();    
+}
