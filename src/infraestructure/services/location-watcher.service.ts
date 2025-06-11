596f618f6d5cf16c619d52afa977b6c680e2f9d3
Jela <jela3105@gmail.com>
2025-06-11 11:37:51 -0600

    Listen when safe location is changed

--- src/infraestructure/services/location-watcher.service.ts
+++ src/infraestructure/services/location-watcher.service.ts
@@ -3,6 +3,7 @@ import { Server as SocketIOServer } from "socket.io";
 import { LocationEntry } from "../../domain/entities/location.entity";
 import { SocketService } from "./socket.service";
 import { getSocketEventHandler } from "../websocket/socket-event-handler";
+import { SentEmail } from "../../domain/use-cases/email/sent-email.user-case";
 
 export class LocationWatcherService {
     constructor(
@@ -22,6 +23,7 @@ export class LocationWatcherService {
         devicesRef.on("child_added", (deviceSnap) => {
             const deviceId = deviceSnap.key!;
             this.listenToHistorial(deviceId);
+            this.listenSafeZone(deviceId);
         });
     }
 
@@ -49,5 +51,29 @@ export class LocationWatcherService {
             }
 
         });
+    }
+
+    private listenSafeZone(deviceId: string) {
+        const onSafeZoneRef = db.ref(`devices/${deviceId}`);
+
+        onSafeZoneRef.on("child_changed", (snapshot) => {
+            console.log("detected")
+            const isInSafeZone = snapshot.val() as boolean;
+
+            const io = SocketService.getIO();
+            const socketEventHandler = getSocketEventHandler();
+
+            if (!socketEventHandler) throw new Error("Socket not initialized");
+
+            const socketId = socketEventHandler.rooms.get(deviceId);
+
+            if (socketId) {
+                io.to(socketId).emit("safe-zone-update", {
+                    deviceId: deviceId,
+                    inSafeZone: isInSafeZone,
+                });
+            }
+        });
+
     }
 }
